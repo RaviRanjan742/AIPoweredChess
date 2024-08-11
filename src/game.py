68c8e8944c93cb9418c1e8f900bffaf0f9bcea2c
Ravi Ranjan <ravi2684ranjan@gmail.com>
2024-08-11 14:02:45 +0530

    Before Proper Dragging

--- src/game.py
+++ src/game.py
@@ -1,17 +1,31 @@
 import pygame
 from const import *
+from board import Board
+from dragger import Dragger
 class Game:
     def __init__(self):
-        pass
+        self.board=Board()
+        self.dragger=Dragger()
     
     def show_bg(self,surface):
         for row in range(ROWS):
             for col in range(COLS):
                 if(row+col)%2==0:
-                    color=(255,255,255)
+                    color=(144,238,144)
                 else:
-                    color=(0,0,0)
+                    color=(255,255,255)
 
                 rect=(col*SQSIZE,row*SQSIZE,SQSIZE,SQSIZE) 
 
                 pygame.draw.rect(surface,color,rect)
+
+    def show_pieces(self,surface):
+        for row in range(ROWS):
+            for col in range(COLS):
+                if self.board.squares[row][col].has_piece():
+                   piece=self.board.squares[row][col].piece
+                   img=pygame.image.load(piece.texture)
+                   img_centre=col * SQSIZE+SQSIZE // 2,row*SQSIZE+SQSIZE //2
+                   piece.texture_rect=img.get_rect(center=img_centre)
+                   surface.blit(img,piece.texture_rect)
+        
--- src/main.py
+++ src/main.py
@@ -9,12 +9,36 @@ class Main:
         pygame.display.set_caption('Chess')
         self.game=Game()
     def mainloop(self):
+        screen=self.screen
+        game=self.game
+        board=self.game.board
+        dragger=self.game.dragger
         while True:
-            screen=self.screen
-            game=self.game
+         
             game.show_bg(screen)
+            game.show_pieces(screen)
             for event in pygame.event.get():
-                if event.type == pygame.QUIT:
+                #click
+                if event.type==pygame.MOUSEBUTTONDOWN:
+                    dragger.update_mouse(event.pos)
+                    clicked_row=dragger.mouseY // SQSIZE
+                    clicked_col=dragger.mouseX // SQSIZE
+
+                    #if clicked square has a piece
+                    if board.squares[clicked_row][clicked_col].has_piece():
+                        piece=board.squares[clicked_row][clicked_col].piece
+                        dragger.save_initial(event.pos)
+                        dragger.drag_piece(piece)
+                #mouse motion
+                elif event.type==pygame.MOUSEMOTION:
+                    if dragger.dragging:
+                        dragger.update_mouse(event.pos)
+                        dragger.update_blit(screen)
+                #click release
+                elif event.type==pygame.MOUSEBUTTONUP:
+                    dragger.undrag_piece()
+                #quit application
+                elif event.type == pygame.QUIT:
                     pygame.quit()
                     sys.exit()
             pygame.display.update()       
