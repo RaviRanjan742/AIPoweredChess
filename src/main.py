7a60a0ecb1c0bec77f23ba688c63658ec3f8269a
Ravi Ranjan <ravi2684ranjan@gmail.com>
2024-08-11 11:41:55 +0530

    Before adding images and sound

--- src/main.py
+++ src/main.py
@@ -1,13 +1,18 @@
 import pygame
 import sys
 from const import * 
+from game import Game
 class Main:
     def __init__(self):
         pygame.init()
         self.screen=pygame.display.set_mode((WIDTH,HEIGHT))
         pygame.display.set_caption('Chess')
+        self.game=Game()
     def mainloop(self):
         while True:
+            screen=self.screen
+            game=self.game
+            game.show_bg(screen)
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     pygame.quit()
